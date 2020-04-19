cbaa42d81c0c9c60f6b955e160aa3027cba20fe3
Minh Le <lenguyetminh1998@gmail.com>
2020-04-18 20:08:26 -0400

    Add files

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <app-menu-view></app-menu-view>
+    <app-main-view></app-main-view>
+   
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+import Main from './components/Main.vue'
+import Menu from './components/Menu.vue'
+
+export default {
+  name: 'App', 
+  components: {
+    'app-main-view': Main, 
+    'app-menu-view': Menu,  
+  }
+}
+
+
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 5em; 
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,35 @@
+<template>
+  
+    <div class="menu">
+       <img class="icon" src="https://img.icons8.com/android/24/000000/menu.png"/> 
+    </div>
+    
+
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.menu{
+    position: fixed;
+    top: 0; 
+    right: 0; 
+
+}
+.icon{
+    padding-top: 1em; 
+    padding-right: 1em; 
+    width: 20px; 
+}
+
+</style>
